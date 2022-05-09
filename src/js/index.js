59b55f471c92c9967afbac1bd811a7e4fb3fdabf
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-05-09 19:21:51 +0300

    feat: implement mouse events

--- src/js/index.js
+++ src/js/index.js
@@ -13,92 +13,11 @@ let shift = "off";
 let cntrl = "off";
 let alt = "off";
 
-console.log(lastLanguage);
 initKeys(lastLanguage);
 
-document.addEventListener("keydown", (e) => {
-    let curKey = document.querySelectorAll(".keybord-key");
-    curKey.forEach((el) => {
-        if (el.getAttribute("data") === e.code) {
-            el.classList.add("active");
-            if (
-                el.className.includes("key-letters") ||
-                el.className.includes("key-space") ||
-                el.className.includes("key-number") ||
-                el.className.includes("key-symbol")
-            ) {
-                TEXTAREA.textContent += el.textContent;
-            }
-        }
-    });
-    if (e.key === "CapsLock") {
-        if (capslock === "on") {
-            capslock = "off";
-            let curKey = document.querySelectorAll(".key-letters");
-            curKey.forEach((el) => {
-                let curText = el.textContent;
-                el.textContent = curText.toLowerCase();
-            });
-        } else if ((capslock = "off")) {
-            capslock = "on";
-            let curKey = document.querySelectorAll(".key-letters");
-            curKey.forEach((el) => {
-                let curText = el.textContent;
-                el.textContent = curText.toUpperCase();
-            });
-        }
-    }
-    if (e.key === "Shift") {
-        shift = "on";
-        // let curKey = document.querySelectorAll(".keybord-key");
-        // curKey.forEach((el) => {
-        //     if (el.getAttribute("data") === e.code) {
-        //         el.classList.add("active");
-        //     }
-        // });
-        if (shift === "on") {
-            initShiftKeys(lastLanguage);
-        }
-    }
-    if (e.key === "Control") {
-        cntrl = "on";
-        if (alt === "on") {
-            let curLang = lastLanguage;
-            if (lastLanguage === "EN") {
-                curLang = "UA";
-                lastLanguage = curLang;
-                localStorage.setItem("app_lang", curLang);
-                initKeys(lastLanguage);
-            } else {
-                curLang = "EN";
-                lastLanguage = curLang;
-                localStorage.setItem("app_lang", curLang);
-                initKeys(lastLanguage);
-            }
-        }
-    }
-    if (e.key === "Alt") {
-        alt = "on";
-        e.preventDefault();
-        if (cntrl === "on") {
-            let curLang = lastLanguage;
-            if (lastLanguage === "EN") {
-                curLang = "UA";
-                lastLanguage = curLang;
-                localStorage.setItem("app_lang", curLang);
-                initKeys(lastLanguage);
-            } else {
-                curLang = "EN";
-                lastLanguage = curLang;
-                localStorage.setItem("app_lang", curLang);
-                initKeys(lastLanguage);
-            }
-        }
-    }
-    if (e.key === "Tab") {
-        e.preventDefault();
-    }
-});
+document.addEventListener("keydown", (e) => press(e));
+
+document.addEventListener("click", (e) => press(e));
 
 document.addEventListener("keyup", (e) => {
     let curKey = document.querySelectorAll(".keybord-key");
@@ -118,3 +37,124 @@ document.addEventListener("keyup", (e) => {
         }
     });
 });
+
+document.addEventListener("mouseover", (e) => {
+    let curKey = document.querySelectorAll(".keybord-key");
+    curKey.forEach((el) => {
+        if (el === e.target) {
+            el.classList.add("hover");
+        }
+    });
+});
+
+document.addEventListener("mouseout", (e) => {
+    let curKey = document.querySelectorAll(".keybord-key");
+    curKey.forEach((el) => {
+        if (el.getAttribute("data") === e.code || el === e.target) {
+            el.classList.remove("active");
+            el.classList.remove("hover");
+        }
+        if (el.getAttribute("data") === "ShiftLeft") {
+            shift = "off";
+            initKeys(lastLanguage);
+        }
+        if (el.getAttribute("data") === "ControlLeft") {
+            cntrl = "off";
+        }
+        if (el.getAttribute("data") === "AltLeft") {
+            alt = "off";
+        }
+    });
+});
+
+function press(e) {
+    let curKey = document.querySelectorAll(".keybord-key");
+    curKey.forEach((el) => {
+        if (el.getAttribute("data") === e.code || el === e.target) {
+            if (el === e.target) {
+                el.classList.remove("hover");
+            }
+            el.classList.add("active");
+            if (
+                el.className.includes("key-letters") ||
+                el.className.includes("key-space") ||
+                el.className.includes("key-number") ||
+                el.className.includes("key-symbol")
+            ) {
+                let currentValue = [...TEXTAREA.value];
+                currentValue.push(el.textContent);
+                currentValue = currentValue.join("");
+                TEXTAREA.value = currentValue;
+            }
+            if (el.getAttribute("data") === "CapsLock") {
+                if (capslock === "on") {
+                    capslock = "off";
+                    let curKey = document.querySelectorAll(".key-letters");
+                    curKey.forEach((el) => {
+                        let curText = el.textContent;
+                        el.textContent = curText.toLowerCase();
+                    });
+                } else if ((capslock = "off")) {
+                    capslock = "on";
+                    let curKey = document.querySelectorAll(".key-letters");
+                    curKey.forEach((el) => {
+                        let curText = el.textContent;
+                        el.textContent = curText.toUpperCase();
+                    });
+                }
+            }
+            if (el.getAttribute("data") === "ShiftLeft") {
+                shift = "on";
+                el.classList.add("active");
+                if (shift === "on") {
+                    initShiftKeys(lastLanguage);
+                }
+            }
+            if (el.getAttribute("data") === "ControlLeft") {
+                cntrl = "on";
+                if (alt === "on") {
+                    let curLang = lastLanguage;
+                    if (lastLanguage === "EN") {
+                        curLang = "UA";
+                        lastLanguage = curLang;
+                        localStorage.setItem("app_lang", curLang);
+                        initKeys(lastLanguage);
+                    } else {
+                        curLang = "EN";
+                        lastLanguage = curLang;
+                        localStorage.setItem("app_lang", curLang);
+                        initKeys(lastLanguage);
+                    }
+                }
+            }
+            if (el.getAttribute("data") === "AltLeft") {
+                alt = "on";
+                e.preventDefault();
+                if (cntrl === "on") {
+                    let curLang = lastLanguage;
+                    if (lastLanguage === "EN") {
+                        curLang = "UA";
+                        lastLanguage = curLang;
+                        localStorage.setItem("app_lang", curLang);
+                        initKeys(lastLanguage);
+                    } else {
+                        curLang = "EN";
+                        lastLanguage = curLang;
+                        localStorage.setItem("app_lang", curLang);
+                        initKeys(lastLanguage);
+                    }
+                }
+            }
+            if (el.getAttribute("data") === "Tab") {
+                e.preventDefault();
+            }
+            if (el.getAttribute("data") === "Backspace") {
+                let currentValue = [...TEXTAREA.value];
+                console.log(currentValue);
+                currentValue.pop();
+                currentValue = currentValue.join("");
+                TEXTAREA.value = currentValue;
+            }
+        }
+    });
+}
